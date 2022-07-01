b69dbc181e60703469a3ce06cef9f1f3a8fad0f0
Thomas Deniffel <tdeniffel@gmail.com>
2022-07-01 15:58:37 +0200

    collected all infos

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module deniffel.com/go_proto_couchdb_store
 
 go 1.18
 
-require google.golang.org/protobuf v1.28.0
+require (
+	github.com/gofrs/uuid v4.2.0+incompatible
+	github.com/satori/go.uuid v1.2.0
+	google.golang.org/protobuf v1.28.0
+)
+
+require gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
--- main/demo.go
+++ main/demo.go
@@ -1,10 +1,40 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	uuid "github.com/satori/go.uuid"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+type User struct {
+	ID    uuid.UUID
+	Realm string
+}
 
 func main() {
+	currentUser := User{
+		ID:    uuid.NewV4(),
+		Realm: "skytala",
+	}
 	p := Person{
 		Name: "Tom",
 	}
-	log.Println(p.String())
+	store(&currentUser, &p)
+}
+
+func store(user *User, message protoreflect.ProtoMessage) {
+	log.Println(user.ID)
+	log.Println(user.Realm)
+	log.Println(message.ProtoReflect().Descriptor().FullName())
+	log.Println(toJson(message))
+}
+
+func toJson(message protoreflect.ProtoMessage) string {
+	encoded, err := protojson.Marshal(message)
+	if err != nil {
+		log.Fatalf("Could not encode proto-mesage: %v", err)
+	}
+	return string(encoded)
 }
